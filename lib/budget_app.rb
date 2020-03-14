18a9f0901835ca1290b11483647e712553cf2c31
Joshua Kleveter <joshua.kleveter@gmail.com>
2020-03-14 18:06:21 -0500

    Connect the db handler to the budget_app

--- lib/budget_app.rb
+++ lib/budget_app.rb
@@ -1,8 +1,12 @@
 # frozen-string-literal: true
 
+require './db_file_handler'
+
 # The main class for the BudgetApp. Instantiate to start the app!
 class BudgetApp
   def initialize(db_file = nil)
+    @db_file_handler = DbFileHandler.new
+
     if db_file
       load_db_file db_file
     else
@@ -14,13 +18,14 @@ class BudgetApp
 
   def create_db_file
     # load in the sqlite3 controller
-    # ask user for the filename
+    # TODO: ask user for the filename
     # create a new db with filename
     # add tables to db
     # done!
+    @db_file_handler.create_db_file 'finance.bdb'
   end
 
   def load_db_file(filename)
-    # TODO
+    @db_file_handler.load_db_file filename
   end
 end
