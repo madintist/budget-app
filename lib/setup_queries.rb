4228162c54aba22235632c93c8d15f11a662f66a
Joshua Kleveter <joshua.kleveter@gmail.com>
2020-03-14 19:48:40 -0500

    Define create table queries

--- lib/setup_queries.rb
+++ lib/setup_queries.rb
@@ -2,6 +2,61 @@
 
 # Queries to set up the database
 class SetupQueries
+  def create_accounts_table
+    <<~SQL
+      CREATE TABLE accounts(
+        id INTEGER,
+        name TEXT,
+        PRIMARY KEY (id)
+      );
+    SQL
+  end
+
+  def create_budgets_table
+    <<~SQL
+      CREATE TABLE budgets(
+        id INTEGER,
+        name TEXT,
+        PRIMARY KEY (id)
+      );
+    SQL
+  end
+
+  def create_statuses_table
+    <<~SQL
+      CREATE TABLE statuses(
+        id INTEGER,
+        name TEXT,
+        PRIMARY KEY (id)
+      );
+
+      INSERT INTO statuses
+        (name)
+      VALUES
+        ('COMPLETE'),
+        ('PENDING');
+    SQL
+  end
+
+  def create_transactions_table
+    <<~SQL
+      CREATE TABLE transactions(
+        id INTEGER,
+        date TEXT,
+        merchant TEXT,
+        account_id INTEGER,
+        budget_id INTEGER,
+        status_id INTEGER,
+        debit REAL,
+        credit REAL,
+        PRIMARY KEY (id),
+        FOREIGN KEY (account_id) REFERENCES accounts(id),
+        FOREIGN KEY (budget_id) REFERENCES budgets(id),
+        FOREIGN KEY (status_id) REFERENCES statuses(id)
+      );
+    SQL
+  end
+
   def enable_foreign_keys
     <<~SQL
       PRAGMA foreign_keys = ON;
