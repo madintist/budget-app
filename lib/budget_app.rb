3656b61497429c70084d44a3be81f48ea93ab68c
Joshua Kleveter <joshua.kleveter@gmail.com>
2020-03-19 07:23:05 -0500

    Removed excess private methods from BudgetApp.

--- lib/budget_app.rb
+++ lib/budget_app.rb
@@ -8,19 +8,9 @@ class BudgetApp
     @db_file_handler = DbFileHandler.new
 
     if db_file
-      load_db_file db_file
+      @db_file_handler.load_db_file filename
     else
-      create_db_file
+      @db_file_handler.create_db_file 'finance.bdb'
     end
-  end
-
-  private
-
-  def create_db_file
-    @db_file_handler.create_db_file 'finance.bdb'
-  end
-
-  def load_db_file(filename)
-    @db_file_handler.load_db_file filename
   end
 end
