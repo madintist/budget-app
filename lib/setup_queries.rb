7ccb93ff76177ae73586fc760596c48ae04b8567
Joshua Kleveter <joshua.kleveter@gmail.com>
2020-03-18 20:43:50 -0500

    Define creation queries

--- lib/setup_queries.rb
+++ lib/setup_queries.rb
@@ -2,5 +2,80 @@
 
 # Queries to set up the database
 class SetupQueries
-  # TODO
+  def create_account_categories
+    <<~SQL
+      CREATE TABLE account_categories(
+        id INTEGER,
+        name TEXT,
+        PRIMARY KEY (id),
+        UNIQUE (name)
+      );
+    SQL
+  end
+
+  def create_accounts
+    <<~SQL
+      CREATE TABLE accounts(
+        id INTEGER,
+        name TEXT,
+        category_id INTEGER,
+        PRIMARY KEY (id),
+        FOREIGN KEY (category_id) REFERENCES account_categories(id),
+        UNIQUE (name)
+      );
+    SQL
+  end
+
+  def create_transaction_statuses
+    <<~SQL
+      CREATE TABLE transaction_statuses(
+        id INTEGER,
+        name TEXT,
+        PRIMARY KEY (id),
+        UNIQUE (name)
+      );
+    SQL
+  end
+
+  def create_transactions
+    <<~SQL
+      CREATE TABLE transactions(
+        id INTEGER,
+        date TEXT,
+        merchant TEXT,
+        debit_account_id INTEGER,
+        credit_account_id INTEGER,
+        status_id INTEGER,
+        amount REAL,
+        PRIMARY KEY (id),
+        FOREIGN KEY (debit_account_id) REFERENCES accounts(id),
+        FOREIGN KEY (credit_account_id) REFERENCES accounts(id),
+        FOREIGN KEY (status_id) REFERENCES transaction_statuses(id),
+        CHECK (amount >= 0)
+      );
+    SQL
+  end
+
+  def insert_account_categories
+    <<~SQL
+      INSERT INTO account_categories
+        (name)
+      VALUES
+        ('Asset'),
+        ('Liability'),
+        ('Income'),
+        ('Expense'),
+        ('Capital');
+    SQL
+  end
+
+  def insert_transaction_statuses
+    <<~SQL
+      INSERT INTO transaction_statuses
+        (name)
+      VALUES
+        ('COMPLETE'),
+        ('PENDING');
+    SQL
+  end
 end
