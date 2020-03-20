fc6c96438d163989185efe2b7332410883c0a1ce
Joshua Kleveter <josh@luckyorange.com>
2020-03-19 21:47:51 -0500

    Use named parameter for BudgetApp.new

--- lib/budget_app.rb
+++ lib/budget_app.rb
@@ -4,7 +4,7 @@ require_relative './budget_database'
 
 # The main class for the BudgetApp. Instantiate to start the app!
 class BudgetApp
-  def initialize(db_filename = nil)
+  def initialize(db_filename: nil)
     @budget_database = BudgetDatabase.new db_filename
   end
 end
