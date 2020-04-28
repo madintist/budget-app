c1428e965a6f18cd279864b0b977900d1b87d6fb
Joshua Kleveter <josh@luckyorange.com>
2020-04-27 22:43:03 -0500

    Correctly set the name of an Account instance

--- lib/account.rb
+++ lib/account.rb
@@ -1,7 +1,7 @@
 class Account
   attr_accessor :name
 
-  def initialize
-    @name = ''
+  def initialize(name)
+    @name = name
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,13 +1,19 @@
 require './lib/account'
 
+account_name = 'Test Account'
+
 describe Account do
   before :each do
-    @test_account = Account.new
+    @test_account = Account.new account_name
   end
 
   describe '#name' do
     it 'is a string' do
       expect(@test_account.name).to be_a(String)
+    end
+
+    it 'is the name that we set with .new' do
+      expect(@test_account.name).to be(account_name)
     end
   end
 end
