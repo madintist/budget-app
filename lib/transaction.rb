46c22cf402d413e37da98b6cf101f59a88f625d5
Joshua Kleveter <josh@luckyorange.com>
2020-04-28 20:22:55 -0500

    Transaction#amount is numeric

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,7 +1,9 @@
 class Transaction
+  attr_reader :amount
   attr_reader :description
 
   def initialize
+    @amount = 0
     @description = ''
   end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -5,6 +5,12 @@ describe Transaction do
     @test_transaction = Transaction.new
   end
 
+  describe '#amount' do
+    it 'is numeric' do
+      expect(@test_transaction.amount).to be_a(Numeric)
+    end
+  end
+
   describe '#description' do
     it 'is a string' do
       expect(@test_transaction.description).to be_a(String)
