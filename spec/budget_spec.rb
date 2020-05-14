e224f5856271081fa9d0096dfeb814bf9cbb1776
Joshua Kleveter <joshua.kleveter@gmail.com>
2020-05-14 18:24:38 -0500

    Now we compile

---
+++ spec/budget_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require './lib/budget'
+
+RSpec.describe Budget do
+  context 'creating a budget' do
+    it 'adds a new budget to the available budgets' do
+    end
+  end
+end
