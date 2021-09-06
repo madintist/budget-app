abcb65d283ffbc8cdd8deef2a9e416c552d60302
Joshua Kleveter <madintist@protonmail.com>
2021-09-05 21:53:18 -0500

    Add type col to accounts

--- src/sql/accounts.sql
+++ src/sql/accounts.sql
@@ -1,5 +1,6 @@
 -- Create the accounts table
 CREATE TABLE IF NOT EXISTS accounts (
 	id INTEGER PRIMARY KEY,
-	name TEXT
+	name TEXT,
+	type INTEGER -- Asset / Liability / etc. will be mapped based on integers
 );
