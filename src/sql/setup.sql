bc39a89fd5d3e17a2fedd338ef840a75a9650166
Joshua Kleveter <madintist@protonmail.com>
2021-09-06 07:00:59 -0500

    Add budgets table

--- src/sql/setup.sql
+++ src/sql/setup.sql
@@ -7,12 +7,19 @@ CREATE TABLE IF NOT EXISTS accounts (
 	initial_balance DECIMAL(10, 2) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS budgets (
+	id INTEGER PRIMARY KEY NOT NULL,
+	name TEXT NOT NULL,
+	initial_balance DECIMAL(10, 2) NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS transactions (
 	id INTEGER PRIMARY KEY NOT NULL,
 	date INTEGER DEFAULT CURRENT_DATE NOT NULL,
 	transaction_number INTEGER, -- Used for check numbers, primarily
 	description VARCHAR(255) NOT NULL,
 	account_id INTEGER NOT NULL,
+	budget_id INTEGER NOT NULL,
 	reconciled BOOLEAN DEFAULT FALSE NOT NULL,
 	credit DECIMAL(10, 2) NOT NULL,
 	debit DECIMAL(10, 2) NOT NULL,
@@ -21,4 +28,9 @@ CREATE TABLE IF NOT EXISTS transactions (
 		REFERENCES accounts (id)
 			ON DELETE RESTRICT
 			ON UPDATE CASCADE
+
+	FOREIGN KEY (budget_id)
+		REFERENCES budgets (id)
+		ON DELETE RESTRICT
+		ON UPDATE CASCADE
 );
